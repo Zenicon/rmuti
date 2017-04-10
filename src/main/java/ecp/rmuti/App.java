744fc75de8715412ada225abc28197a117a24d2f
Ekaphong Tangtrakul <t.ekaphong@gmail.com>
2017-04-10 11:15:46 +0700

    master

---
+++ src/main/java/ecp/rmuti/App.java
@@ -0,0 +1,13 @@
+package ecp.rmuti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
